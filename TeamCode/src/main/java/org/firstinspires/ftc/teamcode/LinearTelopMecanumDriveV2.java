eb9bb37c228215cae63d3fe03d413294d2a8b4e9
PSMV <Vaibhavpradeep777@gmail.com>
2024-10-01 21:12:30 -0400

    Changes 10/01/2024

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearTelopMecanumDriveV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearTelopMecanumDriveV2.java
@@ -1,13 +1,57 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "LinearTelopMecanumDriveV2")
-public class LinearTelopMecanumDriveV2 extends LinearOpMode {
+public class LinearTelopMecanumDriveV2 extends OpMode {
+
+    DcMotor RFMotor;
+    DcMotor LFMotor;
+    DcMotor RBMotor;
+    DcMotor LBMotor;
+    public void moveDriveTrain() {
+        double vertical;
+        double horizontal;
+        double pivot;
+
+        vertical = -gamepad1.left_stick_y;
+        horizontal = gamepad1.left_stick_x;
+        pivot = gamepad1.right_stick_x;
+
+        RFMotor.setPower(pivot + (-vertical) + horizontal);
+        RBMotor.setPower(pivot + (-vertical) - horizontal);
+        LFMotor.setPower(pivot + (-vertical) - horizontal);
+        LBMotor.setPower(pivot + (-vertical) + horizontal);
+
+    }
+
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
+        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+
+        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        RFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LFMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void loop() {
 
     }
 }
